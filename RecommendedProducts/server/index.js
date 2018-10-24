5ae1785c489498b4dcc827b1d7a92bafba360f4c
Faetheon <nateohill@gmail.com>
2018-10-23 17:29:31 -0700

    Initialized basic files for my server and compiler

---
+++ RecommendedProducts/server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
