f6f3ca8552013de6126cbf6ef332904704288b0b
Faetheon <nateohill@gmail.com>
2018-10-30 14:59:39 -0700

    Polished up my seed script to create it's own database if one doesn't exist

--- RecommendedProducts/database-PostgreSQL/dataGen.js
+++ RecommendedProducts/database-PostgreSQL/dataGen.js
@@ -2,19 +2,52 @@ const Sequelize = require('sequelize');
 const loremIpsum = require('./MOCK_DATA.json');
 const { urls } = require('./IMAGE_URLS.json');
 
-const sequelize = new Sequelize('recommended_products', 'nathan', 'student', {
+let sequelize = new Sequelize('postgres', 'nathan', 'student', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
 function genData() {
-  for (let i = 0; i < urls.length; i++) {
-    let {short_desc, rating, reviews, price} = loremIpsum[i];
-    sequelize.query(
-      `INSERT INTO products (short_desc, image_url, rating, reviews, price, category, purchase_url) VALUES ('${short_desc}', '${urls[i]}', '${rating}', '${reviews}', '${price}', 'Electronics', '${urls[i]}')`,
-      {type: sequelize.QueryTypes.INSERT}
-    );
-  }
+  sequelize.query('CREATE TABLE products (id SERIAL PRIMARY KEY, short_desc VARCHAR, image_url VARCHAR, rating DECIMAL, reviews INTEGER, price VARCHAR, category VARCHAR(255), purchase_url VARCHAR);')
+  .then(data => {
+    for (let i = 0; i < urls.length; i++) {
+      let {short_desc, rating, reviews, price} = loremIpsum[i];
+      sequelize.query(
+        `INSERT INTO products (short_desc, image_url, rating, reviews, price, category, purchase_url) VALUES ('${short_desc}', '${urls[i]}', '${rating}', '${reviews}', '${price}', 'Electronics', '${urls[i]}')`,
+        {type: sequelize.QueryTypes.INSERT}
+      );
+    }
+    return data;
+  });
 };
 
-genData();
+sequelize.query("DROP DATABASE recommended_products;")
+.then(data => {
+  sequelize.query("CREATE DATABASE recommended_products;")
+    .then(data => {
+      sequelize.close();
+      sequelize = new Sequelize('recommended_products', 'nathan', 'student', {
+        host: 'localhost',
+        dialect: 'postgres'
+      });
+      genData();
+    });
+})
+.catch(err => {
+  const stringErr = JSON.stringify(err.original);
+  if (/3D000/.test(stringErr)) {
+    console.log('Database not found, creating new database');
+    sequelize.query("CREATE DATABASE recommended_products;")
+    .then(data => {
+      sequelize.close();
+      sequelize = new Sequelize('recommended_products', 'nathan', 'student', {
+        host: 'localhost',
+        dialect: 'postgres'
+      });
+      genData();
+    });
+  } else {
+    throw err;
+  }
+});
+
